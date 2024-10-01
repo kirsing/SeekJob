0cf6f58137f037db88583f92672b724372aa8c2d
Kirill <kirsing98@gmail.com>
2024-10-01 22:19:35 +0300

    init

---
+++ src/main/java/com/kirsing/seekjob/SeekJobApplication.java
@@ -0,0 +1,13 @@
+package com.kirsing.seekjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeekJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeekJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kirsing/seekjob/SeekJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirsing.seekjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeekJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
