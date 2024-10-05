7d9a71b62f60568b05a6d101fa8ea89b28effb34
Kirill <kirsing98@gmail.com>
2024-10-05 12:12:06 +0300

    UsersTypeService hql

--- src/main/java/com/kirsing/seekjob/service/UsersTypeService.java
+++ src/main/java/com/kirsing/seekjob/service/UsersTypeService.java
@@ -1,8 +1,10 @@
 package com.kirsing.seekjob.service;
 
+import com.kirsing.seekjob.entity.Users;
 import com.kirsing.seekjob.entity.UsersType;
 import com.kirsing.seekjob.repository.UsersTypeRepository;
 import lombok.AllArgsConstructor;
+import org.hibernate.Session;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,10 +12,25 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class UsersTypeService {
-    private final UsersTypeRepository usersTypeRepository;
 
-
-    public List<UsersType> findAll() {
-        return usersTypeRepository.findAll();
+    public List<Users> findAll(Session session) {
+        return session.createQuery("select u from Users u", Users.class)
+                .list();
     }
-}
+
+    public Users findUserByEmail(Session session, String email) {
+        return session.createQuery("select u from Users u " +
+                "where u.email = :email", Users.class)
+                .setParameter("email", email)
+                .uniqueResult();
+    }
+
+    public List<Users> findLimitedUsersOrderedByRegistrationDate(Session session, int limit) {
+        return session.createQuery("select u from Users u order by u.registrationDate", Users.class)
+                .setMaxResults(limit)
+                .list();
+    }
+
+
+    }
+
