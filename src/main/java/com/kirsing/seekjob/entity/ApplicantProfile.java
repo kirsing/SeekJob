0a910dd574293bc93d05228395505bd8eaddb914
Kirill <kirsing98@gmail.com>
2024-10-08 14:00:12 +0300

    add more entities: applicant profile, recruiter and skills

---
+++ src/main/java/com/kirsing/seekjob/entity/ApplicantProfile.java
@@ -0,0 +1,39 @@
+package com.kirsing.seekjob.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "applicant_profile")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ApplicantProfile {
+
+    @Id
+    private int userAccountId;
+
+    @OneToOne
+    @JoinColumn(name = "user_account_id")
+    @MapsId
+    private Users userId;
+
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String state;
+    private String country;
+    private String workAuthorization;
+    private String employmentType;
+    private String resume;
+    @Column(nullable = true, length = 64)
+    private String profilePhoto;
+
+    @OneToMany(targetEntity = Skills.class, cascade = CascadeType.ALL
+    , mappedBy = "applicant_profile")
+    private List<Skills> skills;
+}
